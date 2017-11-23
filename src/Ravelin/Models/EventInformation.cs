388d7e62c7f9bae288b68756b0911d234a3a5f0a
Archie Austria <32629421+auaustria@users.noreply.github.com>
2017-11-23 11:32:48 +0800

    added event ticket data models based on Ravelin api update - https://developer.ravelin.com/v2/#event-ticket

---
+++ src/Ravelin/Models/EventInformation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ravelin.Models
+{
+	public class EventInformation
+	{
+		/// <summary>
+		/// A unique identifier for this event.
+		/// </summary>
+		public string EventId { get; set; }
+
+		/// <summary>
+		/// The name of the event.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// A short description of the event.
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Timestamp at which event is scheduled to start (unix time). If the event is ongoing/recurring, you can use the ticket time here.
+		/// </summary>
+		public DateTime? StartTimeUtc { get; set; }
+
+		/// <summary>
+		/// Timestamp at which event is scheduled to end (unix time). If the event is ongoing/recurring, you can use the ticket time here.
+		/// </summary>
+		public DateTime? EndTimeUtc { get; set; }
+
+		/// <summary>
+		/// The category that best described the type of event. One of: sport music attraction conference convention party festival
+		/// </summary>
+		public string Category { get; set; }
+
+		/// <summary>
+		/// The location the event will take place.
+		/// </summary>
+		public Venue Venue { get; set; }
+	}
+}
---
+++ src/Ravelin/Models/Venue.cs
@@ -0,0 +1,15 @@
+﻿namespace Ravelin.Models
+{
+	public class Venue
+	{
+		/// <summary>
+		/// Name of the venue.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Location of the venue.
+		/// </summary>
+		public Location Location { get; set; }
+	}
+}
