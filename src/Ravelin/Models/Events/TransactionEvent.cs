f290b75a0a9a3da287b6a8826442750aa70a54b7
Archie Austria <32629421+auaustria@users.noreply.github.com>
2017-12-12 10:44:53 +0800

    created a base class for TransactionEvent and PreTransactionEvent to reuse same fields

--- src/Ravelin/Models/Events/TransactionEvent.cs
+++ src/Ravelin/Models/Events/TransactionEvent.cs
@@ -1,33 +1,7 @@
 ﻿namespace Ravelin.Models.Events
 {
-	public class TransactionEvent : Event, IPaymentMethodEvent
+	public class TransactionEvent : TransactionEventBase
 	{
-		/// <summary>
-		/// Customer performing the transaction
-		/// </summary>
-		public string CustomerId { get; set; }
-
-		/// <summary>
-		/// A session ID, if the customer has not logged in yet
-		/// </summary>
-		public string TempCustomerId { get; set; }
-
-		/// <summary>
-		/// Payment method used for this transaction
-		/// </summary>
-		public PaymentMethod PaymentMethod { get; set; }
-
-		/// <summary>
-		/// If a payment method was already added for this user in a previous event, it can be referenced here.
-		/// Mutually exclusive with the PaymentMethod field.
-		/// </summary>
-		public string PaymentMethodId { get; set; }
-
-		/// <summary>
-		/// Order this transaction is (partially) resolving
-		/// </summary>
-		public string OrderId { get; set; }
-
 		/// <summary>
 		/// The transaction which was committed to the PSP, including the response from the PSP
 		/// </summary>
